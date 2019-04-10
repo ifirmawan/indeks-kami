079d06c33c85b81da23b70f32d7efedef65931cf
firmawaneiwan@gmail.com <firmawan@firmawan-PC-VK29HDZCE>
2019-04-10 17:17:21 +0700

    edit tentang kami

--- resources/views/tentang_kami.blade.php
+++ resources/views/tentang_kami.blade.php
@@ -1,21 +1,34 @@
 @extends('layouts.dashboard-kami')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
+<div class="row">
+    <div class="col-md-12">
+        <div class="page-header">
+            <h2 class="pageheader-title">Indeks Keamanan Informasi </h2>
+            <div class="section-block" id="basicform" tabindex="-1">
+                <p>
+                Versi 3.1, 15 April 2015
+                </p>
+
+            </div>
+        </div>
+    </div>
+</div>
+<div class="ecommerce-widget">
+
+    <div class="row">
+        <div class="col-md-12">
             <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
+                    <div class="card-header d-flex">
+                   <h3>Tentang Kami</h3>     
+                    </div>
+                    <div class="card-body">
+                        <p>Indeks KAMI adalah alat evaluasi untuk menganalisa tingkat kesiapan pengamanan informasi di Instansi pemerintah. Alat evaluasi ini tidak ditujukan untuk menganalisa kelayakan atau efektifitas bentuk pengamanan yang ada, melainkan sebagai perangkat untuk memberikan gambaran kondisi kesiapan (kelengkapan dan kematangan) kerangka kerja keamanan informasi kepada pimpinan Instansi. Evaluasi dilakukan terhadap berbagai area yang menjadi target penerapan keamanan informasi dengan ruang lingkup pembahasan yang juga memenuhi semua aspek keamanan yang didefinisikan oleh standar ISO/IEC 27001:2013. 
+                                Bentuk evaluasi yang diterapkan dalam indeks KAMI dirancang untuk dapat digunakan oleh Instansi pemerintah dari berbagai tingkatan, ukuran, maupun tingkat kepentingan penggunaan TIK dalam mendukung terlaksananya Tugas Pokok dan Fungsi yang ada. Data yang digunakan dalam evaluasi ini nantinya akan memberikan snapshot indeks kesiapan - dari aspek kelengkapan maupun kematangan - kerangka kerja keamanan informasi yang diterapkan dan dapat digunakan sebagai pembanding dalam rangka menyusun langkah perbaikan dan penetapan prioritasnya.  
+                                Alat evaluasi ini kemudian bisa digunakan secara berkala untuk mendapatkan gambaran perubahan kondisi keamanan informasi sebagai hasil dari program kerja yang dijalankan, sekaligus sebagai sarana untuk menyampaikan peningkatan kesiapan kepada pihak yang terkait (stakeholders). 
+                                Penggunaan dan publikasi hasil evaluasi Indeks KAMI merupakan bentuk tanggungjawab penggunaan dana publik sekaligus menjadi sarana untuk meningkatkan kesadaran mengenai kebutuhan keamanan informasi di Instansi pemerintah. Pertukaran informasi dan diskusi dengan Instansi pemerintah lainnya sebagai bagian dari penggunaan alat evaluasi Indeks KAMI ini juga menciptakan alur komunikasi antar pengelola keamanan informasi di sektor pemerintah sehingga semua pihak dapat mengambil manfaat dari lesson learned yang sudah dilalui.
+                                </p>
+                    </div>
             </div>
         </div>
     </div>
