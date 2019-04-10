0f3d19c83e595346356ded446529b3611b50a0dc
firmawaneiwan@gmail.com <firmawan@firmawan-PC-VK29HDZCE>
2019-04-10 16:39:38 +0700

    set table name in KategoriSE Model

--- app/KategoriSE.php
+++ app/KategoriSE.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class KategoriSE extends Model
 {
+    protected $table = 'kategori_s_es';
     public function parameter()
     {
         return $this->belongsTo('App\Parameter','parameter_id','id');
